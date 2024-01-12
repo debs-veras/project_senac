1fdfc4bf632985e97249096587af463395beab23
Débora Hellen <deboraveras.inf1.ep@outlook.com>
2024-01-12 17:23:26 -0300

    inicio do curso detail

--- src/app/pages/detailcurso/detailcurso.component.ts
+++ src/app/pages/detailcurso/detailcurso.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CardCurso } from '../../interface/card-curso';
 @Component({
   selector: 'app-detailcurso',
   templateUrl: './detailcurso.component.html',
   styleUrl: './detailcurso.component.css'
 })
-export class DetailcursoComponent {
+export class DetailcursoComponent implements OnInit{
+  curso !: CardCurso
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.curso = {
+        id: params['id'],
+        name: params['name'],
+        type: params['type'],
+        modality: params['modality'],
+        duration: params['duration'],
+        typecurso: params['typecurso'],
+        urlimg: params['urlimg'],
+        turno: params['turno'],
+        cor: params['cor']
+      };
+    });
+  }
 }
--- src/app/pages/detailscategory/detailscategory.component.ts
+++ src/app/pages/detailscategory/detailscategory.component.ts
@@ -25,7 +25,6 @@ export class DetailscategoryComponent implements OnInit{
         img2: params['img2']
       };
     });
-    console.log(this.curso.cor)
   }
 
 }
